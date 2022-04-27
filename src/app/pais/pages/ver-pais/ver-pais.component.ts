1994089d0db3a6473dd75b3c7d118940f75b20b0
Juan Carlos Sanchez <j.sanchez@dappertechnologies.com>
2022-04-27 10:16:51 -0500

    Section 8 finished

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -12,26 +12,34 @@ import { Country } from '../../interfaces/pais.interface';
 })
 export class VerPaisComponent implements OnInit {
 
-  paises : Country[] = [];
+  //paises : Country[] = [];
+  pais!: Country;
 
   constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService) { }
 
   ngOnInit(): void {
 
+    /*
+      La funcion devuelve un arreglo, por lo tanto existen dos opciones para arreglar el problema que tiene el curso. La primera es recibir el primer elemento del arreglo y asignarlo a la variable pais (opcion 1). Y la segunda que es crear un array de paises y guardar todo el arreglo recibido en la respuesta dentro de nuestro array creado para después iterarlo en el html utilizando un ng-for.
+    */
+
+    //Opcion 2
     this.activatedRoute.params
     .pipe(
       switchMap( ({id}) => this.paisService.getPaisPorAlpha(id)),
       tap(console.log)
     )
-    .subscribe( paises => this.paises = paises);
+    .subscribe( pais => this.pais = pais[0]);
 
+
+    //Opcion 1
     // this.activatedRoute.params
-    // .subscribe(({id}) => {
-    //   this.paisService.getPaisPorAlpha(id)
-    //   .subscribe( pais => {
+    // .pipe(
+    //   switchMap( ({id}) => this.paisService.getPaisPorAlpha(id)),
+    //   tap(console.log)
+    // )
+    // .subscribe( paises => this.paises = paises);
 
-    //   });
-    // });    
   }
 
 }
